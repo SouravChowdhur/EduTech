85ec27355c20993f49593105c99d47bbabea92c9
SouravChowdhur <96020389+SouravChowdhur@users.noreply.github.com>
2022-09-30 17:13:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+
+        },
+        768: {
+          slidesPerView: 2,
+      
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  var swiper = new Swiper(".faculties-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+
+        },
+        768: {
+          slidesPerView: 2,
+      
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+
+        },
+        768: {
+          slidesPerView: 2,
+      
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
